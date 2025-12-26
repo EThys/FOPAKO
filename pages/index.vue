82d50560bd90658188d8479152eed91d5a1aee71
Ethys <ethbergmuzola500@gmail.com>
2025-12-26 12:26:52 +0100

    Page d'accueil avec tous les composants

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <Hero />
+    <FeatureStrip />
+    <About />
+    <ServicesSolid />
+    <NewsSection />
+    <CtaSection />
+    <BlogSection />
+    <GallerySection />
+    <Partners />
+    <Footer />
+  </main>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: 'FOPAKO - Force Paysanne du Kongo Central',
+  meta: [
+    { name: 'description', content: 'Cultivons l\'avenir du Kongo Central ensemble. Agriculture durable, conscience, unité et travail.' }
+  ]
+})
+</script>
